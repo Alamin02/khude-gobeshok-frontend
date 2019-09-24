c7996d7807deb2b0290d71dd360f059a870fc1f5
Alamin Hossain <alamin.ece.ku@gmail.com>
2019-09-24 17:30:28 +0600

    Navbar Change

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -21,68 +21,82 @@ class Navbar extends React.Component {
 
         return (
             <Menu pointing fixed="top" >
-                <Container>
-                    <Link to={"/"}>
-                        <Menu.Item
-                            icon="home"
-                            name=""
-                            active={activeItem === "home"}
-                            onClick={() => this.handleIconClick("home")}
-                        />
-                    </Link>
 
-                    <Link to={"/projects"}>
-                        <Menu.Item
-                            name="Project Garage"
-                            active={activeItem === "Project Garage"}
-                            onClick={this.handleItemClick}
-                        />
-                    </Link>
+                <Link to={"/"}>
+                    <Menu.Item
+                        icon="home"
+                        name=""
+                        active={activeItem === "home"}
+                        onClick={() => this.handleIconClick("home")}
+                    />
+                </Link>
 
-                    <Menu.Menu position="right">
-                        {
-                            this.props.loggedIn ?
-                                <React.Fragment>
-                                    <Link to="/new-project">
-                                        <Menu.Item
-                                            name="Add Project"
-                                            active={activeItem === "Add Project"}
-                                            onClick={this.handleItemClick}
-                                        />
-                                    </Link>
-                                    <Menu.Item icon='bell' />
-                                    <Menu.Item icon='envelope' />
-                                    <Dropdown item icon="user" pointing="top right" className='link item'>
-                                        <Dropdown.Menu active >
-                                            <Link to={`/profile/` + this.props.username} style={{ textDecoration: "none" }}>
-                                                <Dropdown.Item text='Your Profile' onClick={() => this.handleIconClick("")} />
-                                            </Link>
-                                            <Link to='/logout'>
-                                                <Dropdown.Item text='Logout' onClick={() => this.handleIconClick("")} />
-                                            </Link>
-                                        </Dropdown.Menu>
-                                    </Dropdown>
-                                </React.Fragment>
-                                :
-                                <React.Fragment>
-                                    <Link to="/login">
-                                        <Menu.Item
-                                            name="Login"
-                                            active={activeItem === "Login"}
-                                            onClick={this.handleItemClick}
-                                        />
-                                    </Link>
-                                    <Link to="/signup">
-                                        <Menu.Item
-                                            name="Sign Up"
-                                            active={activeItem === "Sign Up"}
-                                            onClick={this.handleItemClick}
-                                        />
-                                    </Link>
-                                </React.Fragment>
-                        }
-                    </Menu.Menu>
-                </Container>
+                <Link to={"/projects"}>
+                    <Menu.Item
+                        name="Project Garage"
+                        active={activeItem === "Project Garage"}
+                        onClick={this.handleItemClick}
+                    />
+                </Link>
+
+                <Link to={"/projects"}>
+                    <Menu.Item
+                        name="Challenges"
+                        active={activeItem === "Challenges"}
+                        onClick={this.handleItemClick}
+                    />
+                </Link>
+                <Link to={"/projects"}>
+                    <Menu.Item
+                        name="Make Squad"
+                        active={activeItem === "Make Squad"}
+                        onClick={this.handleItemClick}
+                    />
+                </Link>
+
+                <Menu.Menu position="right">
+                    {
+                        this.props.loggedIn ?
+                            <React.Fragment>
+                                <Link to="/new-project">
+                                    <Menu.Item
+                                        name="Add Project"
+                                        active={activeItem === "Add Project"}
+                                        onClick={this.handleItemClick}
+                                    />
+                                </Link>
+                                <Menu.Item icon='bell' />
+                                <Menu.Item icon='envelope' />
+                                <Dropdown item icon="user" pointing="top right" className='link item'>
+                                    <Dropdown.Menu active >
+                                        <Link to={`/profile/` + this.props.username} style={{ textDecoration: "none" }}>
+                                            <Dropdown.Item text='Your Profile' onClick={() => this.handleIconClick("")} />
+                                        </Link>
+                                        <Link to='/logout'>
+                                            <Dropdown.Item text='Logout' onClick={() => this.handleIconClick("")} />
+                                        </Link>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </React.Fragment>
+                            :
+                            <React.Fragment>
+                                <Link to="/login">
+                                    <Menu.Item
+                                        name="Login"
+                                        active={activeItem === "Login"}
+                                        onClick={this.handleItemClick}
+                                    />
+                                </Link>
+                                <Link to="/signup">
+                                    <Menu.Item
+                                        name="Sign Up"
+                                        active={activeItem === "Sign Up"}
+                                        onClick={this.handleItemClick}
+                                    />
+                                </Link>
+                            </React.Fragment>
+                    }
+                </Menu.Menu>
             </Menu>
         );
     }
