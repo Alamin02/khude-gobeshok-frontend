616d17cbb61b976c2177d9f755c3246697795566
Alamin Hossain <alamin.ece.ku@gmail.com>
2019-10-16 12:56:00 +0600

    footer fix

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -8,9 +8,9 @@ export default class Footer extends Component {
             <div>
                 <Segment vertical style={{ padding: '1.5em 0em' }} inverted>
                     <Container textAlign="center">
-                        <Image src='/Logo.png' size='mini' centered />
+                        <Image src='/img/footer-logo.png' size='mini' centered />
                         <br />
-                        <p><Link to="/contact">Contact</Link> | <Link to="/about">About Us</Link> | <Link to="/privacy">Privacy Policy</Link></p>
+                        <p><Link style={{ color: "white" }} to="/contact">Contact</Link> | <Link style={{ color: "white" }} to="/about">About Us</Link> | <Link style={{ color: "white" }} to="/privacy">Privacy Policy</Link></p>
                         <p>Copyright &copy; Khudegobeshok 2019, All Rights Reserved.</p>
                     </Container>
                 </Segment>
