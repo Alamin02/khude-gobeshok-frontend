2a9aeefc3c646a14e42e29a54432d4a041c1bbe8
Md. Al Amin <alamin.ece.ku@gmail.com>
2019-10-23 14:17:57 +0600

    fix message button bug

--- src/components/profile/ProfileRegularInfo.jsx
+++ src/components/profile/ProfileRegularInfo.jsx
@@ -158,7 +158,7 @@ class ProfileRegularInfo extends Component {
 
                     <Divider />
 
-                    {own && <Button fluid as={Link} to={messageLink} color="blue" size="small">MESSAGE</Button>}
+                    {own || <Button fluid as={Link} to={messageLink} color="blue" size="small">MESSAGE</Button>}
                 </Segment>
 
                 <Segment>
